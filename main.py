b408d3731ee4567d6d9219dde778146fb3ad0460
geovana123m <128698692+geovana123m@users.noreply.github.com>
2024-02-15 10:45:57 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+#nome = input("escreva o seu nome")
+#try:
+   # idade = int (input("escreva a sua idade:"))
+#except ValueError:
+    #print("voce nao digitou um numero")
+#idade = int(idade)
+#print(f"ola {nome}, voce tem{idade}anos.")
+
+numero = float (input (escreva um numero))
+
+
