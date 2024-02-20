aa1c2494cc327b227f701b2f0d15e9f25bb925b1
geovana123m <128698692+geovana123m@users.noreply.github.com>
2024-02-20 10:37:46 -0300

    Add files via upload

---
+++ exercicio3.py
@@ -0,0 +1,12 @@
+nota = int(input("Digite sua nota: "))
+if nota >90:
+    categoria = "A"
+elif nota >80:
+    categoria = "B"
+elif nota >70:
+    categoria = "C"
+elif nota >60:
+    categoria = "D"
+else:
+    categoria = "F"
+print(f"Você está na categoria: {categoria}.")
