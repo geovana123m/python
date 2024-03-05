8ac281477ec8c635f5cf9bc0d0a387453a620562
geovana123m <128698692+geovana123m@users.noreply.github.com>
2024-03-05 08:26:02 -0300

    Add files via upload

--- forca.py
+++ forca.py
@@ -1,20 +1,24 @@
-palavra_secreta = "parara"
-letras_acertadas = ["_", "_", "_", "_", "_", "_"]
-tentativas = 8
-while tentativas > 0 and "_" in letras_acertadas:
-    palpite = input("digite uma letra").lower()
-    if palpite in palavra_secreta:
-        index = 0
-        for letra in palavra_secreta:
-            if palpite == letra:
-                letras_acertadas[index] = letra
-            index += 1 
-    else: 
-        tentativas -= 1
-        print (f"voce tem {tentativas} tentativas")
-    print(" ".join(letras_acertadas))
-        
-if "_" not in letras_acertadas:
-    print("parabens, voce ganhou")
-else:
-    print(f"voce perdeu{palpalavra_secreta}")
+def jogar():
+    palavra_secreta = "parara"
+    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+    tentativas = 8
+    while tentativas > 0 and "_" in letras_acertadas:
+        palpite = input("digite uma letra").lower()
+        if palpite in palavra_secreta:
+            index = 0
+            for letra in palavra_secreta:
+                if palpite == letra:
+                    letras_acertadas[index] = letra
+                index += 1 
+        else: 
+            tentativas -= 1
+            print (f"voce tem {tentativas} tentativas")
+        print(" ".join(letras_acertadas))
+            
+    if "_" not in letras_acertadas:
+        print("parabens, voce ganhou")
+    else:
+        print(f"voce perdeu{palpalavra_secreta}")
+
+if (__name__ == "__main__"):
+    jogar()
